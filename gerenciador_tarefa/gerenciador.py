790959e705b4679e9511c203938a175846e2a688
KarinaBertizini <karinabertizini@gmail.com>
2021-11-01 23:13:25 -0200

    Estudo 01-11-21

--- gerenciador_tarefa/gerenciador.py
+++ gerenciador_tarefa/gerenciador.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-TAREFA = []
+
+TAREFA = [
+    {
+        "id": "1",
+        "titulo": "fazer compras",
+        "descrição": "comprar leite e ovos",
+        "estado": "não finalizado",
+    },
+    {
+        "id": "2",
+        "titulo": "levar o cachorro para tosar",
+        "descrição": "está muito peludo",
+        "estado": "não finalizado",
+    },
+    {
+        "id": "3",
+        "titulo": "lavar roupas",
+        "descrição": "estão sujas",
+        "estado": "não finalizado",
+    },
+]
 
 @app.get("/tarefa")
 def listar():
